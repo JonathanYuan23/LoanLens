3a7b318db9a501fa2bf5926a58e756485cff0bdd
rache111he <rachelheo455@gmail.com>
2024-07-09 13:58:07 -0400

    testprod

---
+++ test/testproduction.sql
@@ -0,0 +1,55 @@
+WITH RECURSIVE HouseholdMembers AS (
+    -- Anchor member: the given user
+    SELECT 
+        user_id 
+    FROM 
+        Users 
+    WHERE 
+        user_id = 6
+
+    UNION
+
+    -- Get dependents and providers
+    SELECT 
+        d.dependant_id AS user_id 
+    FROM 
+        Dependant d
+    JOIN 
+        HouseholdMembers hm ON d.provider_id = hm.user_id
+
+    UNION
+
+    SELECT 
+        d.provider_id AS user_id 
+    FROM 
+        Dependant d
+    JOIN 
+        HouseholdMembers hm ON d.dependant_id = hm.user_id
+
+    UNION
+
+    -- Get spouses
+    SELECT 
+        m.spouse_id_2 AS user_id 
+    FROM 
+        Married m 
+    JOIN 
+        HouseholdMembers hm ON m.spouse_id_1 = hm.user_id
+
+    UNION
+
+    SELECT 
+        m.spouse_id_1 AS user_id 
+    FROM 
+        Married m 
+    JOIN 
+        HouseholdMembers hm ON m.spouse_id_2 = hm.user_id
+)
+
+SELECT 
+    SUM(u.income) AS total_household_income
+FROM 
+    HouseholdMembers hm
+JOIN 
+    Users u ON hm.user_id = u.user_id;
+
