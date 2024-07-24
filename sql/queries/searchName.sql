b1cc62cb2813c7bea3521675c1abe926ea9df6b6
rache111he <rachelheo455@gmail.com>
2024-07-24 16:55:57 -0400

    fix searchbyname query

--- sql/queries/searchName.sql
+++ sql/queries/searchName.sql
@@ -1,1 +1,19 @@
-SELECT * FROM Users WHERE name LIKE %<name>%
+SELECT 
+    Users.user_id, 
+    Users.name, 
+    Users.address, 
+    Users.dob, 
+    City.city_name, 
+    Company.company_name, 
+    Job.job_title, 
+    Users.income
+FROM 
+    Users
+JOIN 
+    City ON Users.city_id = City.city_id
+JOIN 
+    Company ON Users.company_id = Company.company_id
+JOIN 
+    Job ON Users.job_id = Job.job_id
+WHERE 
+    Users.name LIKE '%<name>%';
--- test/test_prod/testproduction.sql
+++ test/test_prod/testproduction.sql
@@ -154,7 +154,25 @@ GROUP BY user_id;
 
 -------------------------------------------------------------
 -- searchName.sql
-SELECT * FROM Users WHERE name = 'Tyler Evans' 
+SELECT 
+    Users.user_id, 
+    Users.name, 
+    Users.address, 
+    Users.dob, 
+    City.city_name, 
+    Company.company_name, 
+    Job.job_title, 
+    Users.income
+FROM 
+    Users
+JOIN 
+    City ON Users.city_id = City.city_id
+JOIN 
+    Company ON Users.company_id = Company.company_id
+JOIN 
+    Job ON Users.job_id = Job.job_id
+WHERE 
+    Users.name LIKE '%yler B%';
 
 ---------------------------------------------------------------
 -- userInfo.sql
